e4c23120bd92e61e9a5f7c9fe459b12f16ef3f41
Quentin Mongalvy <quentin.mongalvy@gmail.com>
2025-12-12 18:30:49 +0100

    début

---
+++ Joueur.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projet
+{
+    class Joueur
+    {
+        private string nom;
+        private List<string>? mots;
+        private int score;
+        public Joueur(string nom)
+        {
+            this.nom = nom;
+            mots = null;
+            score = 0;
+        }
+        public string Nom
+        {
+            get{return nom;}
+        }
+        public List<string>? Mots
+        {
+            get{return mots;}
+            set{mots = value;}
+        }
+        public int Score
+        {
+            get{return score;}
+            set{score = value;}
+        }
+        public void Add_Mot(string a)
+        {
+            mots.Add(a);
+        }
+        public string affichermot()
+        {
+            string a = "";
+            for(int i = 0; i < mots.Count; i++)
+            {
+                a = a + mots[i] + "; ";
+            }
+            return a;
+        }
+        public override string ToString()
+        {
+            return $"Le joueurs {nom} a {score} de score est a trouvé les mots suivant : {affichermot()}";
+        }
+        public void Add_Score(int val)
+        {
+            score = score + val;
+        }
+        public bool Contient(string mot)
+        {
+            bool a = false;
+            for(int i = 0; i < mots.Count; i++)
+            {
+                if(mots[i] == mot)
+                {
+                    a = true;
+                    break;
+                }
+            }
+            return a;
+        }
+    }
+}
--- obj/Debug/net9.0/projet.AssemblyInfo.cs
+++ obj/Debug/net9.0/projet.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("projet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b8a2069949c1ebb9eaad05991d72dbb105fa3d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("projet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("projet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
